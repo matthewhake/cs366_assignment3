769987d17f370928b040e048117cd90966486f0e
Matthew Hake <mhake@hamilton.edu>
2025-11-19 18:38:17 -0500

    wrote IMDBDataset class

--- src/data.py
+++ src/data.py
@@ -12,12 +12,46 @@ import numpy as np
 
 pl.seed_everything(42)
 
+# Source: https://www.sabrepc.com/blog/Deep-Learning-and-AI/text-classification-with-bert
+# Use: Explains how to initialize a generic text classification dataset
+class IMDBDataset(Dataset):
+    def __init__(self, data, tokenizer, max_length):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        row = self.data[idx]
+        text = row['text']
+        label = row['label']
+
+        #cut if review is too long. Add blank space if review is too short
+        encoding = self.tokenizer(
+            text,
+            max_length=self.max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt"
+        )
+
+        return {
+            "input_ids": encoding["input_ids"].squeeze(0),
+            "label": torch.tensor(label, dtype=torch.long)
+        }
+
+
 class IMDBDataModule(pl.LightningDataModule):
     def __init__(self, batch_size=32, max_length=256):
         super().__init__()
         self.batch_size = batch_size
         self.max_length = max_length
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
     
     def download_and_split(self):
         #loading imdb dataset
@@ -43,8 +77,7 @@ class IMDBDataModule(pl.LightningDataModule):
         self.val_dataset = val_test_splits['train']
         self.test_dataset = val_test_splits['test']
 
-        def setup(self, stage=None):
-        # We simply call your custom method here
+    def setup(self, stage=None):
         if self.train_dataset is None:
             self.download_and_split()
 
